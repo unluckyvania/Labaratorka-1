c93170bcfd8d4bb41fbc1da679f4e9056753e8ab
unluckyvania <50343870+unluckyvania@users.noreply.github.com>
2026-05-26 16:37:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// СНАЧАЛА основной код программы
+Rectangle rect = new Rectangle();
+rect.height = 2;
+rect.width = 3;
+rect.Show();
+float p = rect.Perimetr();
+Console.WriteLine("Perimeter: " + p);
+Console.ReadKey();
+
+// ПОТОМ объявление класса
+class Rectangle
+{
+    public float height;
+    public float width;
+
+    public void Show()
+    {
+        Console.WriteLine("Rectangle:");
+        Console.WriteLine("  Height: " + height);
+        Console.WriteLine("  Width: " + width);
+    }
+
+    public float Perimetr()
+    {
+        return 2 * (height + width);
+    }
+}
